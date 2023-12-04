12347554579ca000cc991cf473468dc026b487c8
Andrés I. <suurrend3r@gmail.com>
2023-12-03 23:10:44 -0300

    pertag

--- config.h
+++ config.h
@@ -1,11 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
-static const unsigned int gappx     = 5;
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const unsigned int gappx     = 15;	/* gaps between windows */
+
 static const char *fonts[]          = { "monospace:size=11" };
 static const char dmenufont[]       = "monospace:size=11";
 static const char col_gray1[]       = "#222222";
