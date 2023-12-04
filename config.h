1362b0fbbcc6f3b3b4b7116cd0b6807de3adc9df
Andrés I. <suurrend3r@gmail.com>
2023-12-04 12:19:11 -0300

    gimp is floating: 0

--- config.h
+++ config.h
@@ -31,7 +31,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Gimp",     NULL,       NULL,       0,            0,           -1 },
 	{ "Chromium",  NULL,       NULL,       1 << 2,       0,           -1 },
 };
 
