54e629257212bf0bccd729b0462b1a79e3d354c7
Andrés I. <suurrend3r@gmail.com>
2023-12-04 10:39:29 -0300

    bottom bar

--- config.h
+++ config.h
@@ -4,8 +4,8 @@
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const unsigned int gappx     = 15;	/* gaps between windows */
+static const int topbar             = 0;        /* 0 means bottom bar */
+static const unsigned int gappx     = 20;	/* gaps between windows */
 
 static const char *fonts[]          = { "monospace:size=11" };
 static const char dmenufont[]       = "monospace:size=11";
